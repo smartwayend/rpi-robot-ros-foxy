838e8c5c42829adacd2413aaffc84a4c68b8d6ab
smartwayend <134828744+smartwayend@users.noreply.github.com>
2023-06-02 01:23:23 +0900

    Update setup.py

--- rpi_robot_control/setup.py
+++ rpi_robot_control/setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pepe',
+    maintainer='smartway',
     maintainer_email='eduardo.laruta@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
