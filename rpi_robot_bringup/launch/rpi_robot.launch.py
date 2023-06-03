f68130a56f2216892f0794ce0c5296ff4f34e524
smartwayend <134828744+smartwayend@users.noreply.github.com>
2023-06-03 20:31:27 +0900

    Update rpi_robot.launch.py

--- rpi_robot_bringup/launch/rpi_robot.launch.py
+++ rpi_robot_bringup/launch/rpi_robot.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments=['0', '0', '0.015', '0', '0', '0', 'base_link', 'laser'],
+            arguments=['0', '0', '0.150', '0', '0', '0', 'base_link', 'laser'],
             output='screen'
         )
 
